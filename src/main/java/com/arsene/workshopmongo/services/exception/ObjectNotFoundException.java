7971f2bf8f3392fb5a2ce3d8d253e3596a91f259
ArseneTF <angelogus22@gmail.com>
2023-07-19 09:58:13 -0300

    Obtendo um usuário por id

---
+++ src/main/java/com/arsene/workshopmongo/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,9 @@
+package com.arsene.workshopmongo.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+	
+	public ObjectNotFoundException(String msg) {
+		super(msg);
+	}
+}
