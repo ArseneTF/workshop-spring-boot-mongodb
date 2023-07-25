99674c3d99a5589cbaa7bc392d9db627a6637010
ArseneTF <angelogus22@gmail.com>
2023-07-25 09:51:16 -0300

    Obtendo um post por id

---
+++ src/main/java/com/arsene/workshopmongo/services/PostService.java
@@ -0,0 +1,24 @@
+package com.arsene.workshopmongo.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.arsene.workshopmongo.domain.Post;
+import com.arsene.workshopmongo.domain.User;
+import com.arsene.workshopmongo.repository.PostRepository;
+import com.arsene.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+		}
+}
